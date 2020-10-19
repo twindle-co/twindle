ba3f9a4fe9ea4a893928f52224dfd1da03fca3ae
Kingsley Victor <javaprodigy56@gmail.com>
2020-10-19 20:10:19 +0100

    set up server

---
+++ server/api/index.js
@@ -0,0 +1,27 @@
+const { default: Axios } = require("axios");
+
+class API {
+ static async getTwitterThread(req, res) {
+  try {
+   // Obtain variables injected into request alongside the request body. These injected variables are made available by the custom Twitter middleware.
+   const { twitterUrl, twitterApiKey, opts, body } = req;
+   const response = await Axios.get(twitterUrl + "/2/tweets/" + body.id, {
+    headers: {
+     ...opts.headers,
+     "Authorization": `Bearer ${twitterApiKey}`
+    }
+   });
+   res.status(200).json({
+    statusCode: 200,
+    response: response.data
+   });
+  } catch (error) {
+   res.status(500).json({
+    statusCode: 500,
+    response: error.message
+   });
+  }
+ }
+}
+
+module.exports.API = API;
---
+++ server/middlewares/twitter.js
@@ -0,0 +1,26 @@
+const { TWITTER_API_URL, TWITTER_API_KEY } = require("../environment");
+
+module.exports.http = (req, res, next) => {
+ try {
+  // Inject the Twitter api url into every request 
+  req.twitterUrl = TWITTER_API_URL;
+
+  // Inject the Twitter api key into every request
+  req.twitterApiKey = TWITTER_API_KEY;
+
+  // Inject options into every request. These options would be made available as configuration parameters to axios
+  req.opts = {
+   headers: {
+    "Content-Type": "application/json"
+   }
+  };
+
+  // Pass control to the next function
+  next();
+ } catch (error) {
+  res.status(500).json({
+   statusCode: 500,
+   response: error.message
+  });
+ }
+};
