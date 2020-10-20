b3992c776f25e3d2778b4e8b1884ab58ccaeee01
praveen2896 <mpraveenkumarown@gmail.com>
2020-10-20 20:15:43 +0530

    changes in html to pdf creation and code factoring

--- playground/cli/jsonToPDF/script.js
+++ playground/cli/jsonToPDF/script.js
@@ -1,40 +1,58 @@
-const Mustache = require("mustache")
+const Mustache = require("mustache"); 
 const fs = require('fs')
 const createHTML = require('create-html')
-const pdf = require('html-pdf')
+const htmlToPdf = require('html-to-pdf');
 
- 
 main();
-function main(){
+function main(){   
     let data;
-    //getting the json file and then console
-    data= require("./twit_thread.json");
-    console.log(data);
+    //get the data from json
+    data=require("./twit_thread.json"); 
+    //console.log(data);
     let tweetData={
       tweets:data
     }
-    console.log(tweetData);
-    //template for Mustache ,
-    let tweetTemplate='<div> {{#tweets}}<h2>Hello {{tweet}}</h2> <br></br> {{name}}{{/tweets}} </div>'
+    //console.log(tweetData);
+    //get external mustache template 
+    fs.readFile("tweet_template.mustache",'utf8',function(err,data){
+        if(err)
+        {
+            console.log(err);
+            return;
+        }
     //render the View data in the Mustache template
-    let info=Mustache.render(tweetTemplate,tweetData);
-    //console.log("HTML CODE-->",info) 
-    //create an HTML
+    let info=Mustache.render(data,tweetData);
+    //create an HTMLContent
     let htmlContent = createHTML({
         title: 'Twindle',
-        body:info
+        body:info,
+        css:"output.css"
     })
-    console.log("HTML ",htmlContent);
-    // fs.writeFile('output.html', html, function (err) {
-    //   if (err) console.log(err)
-    // })
-    let options = { format: 'Letter' };
-    //convert the HTML Content/HTML Page to PDF
-    pdf.create(htmlContent, options).toFile('./output.pdf', function(err, res) {
-        if (err) return console.log(err);
-        console.log("file Path"+res.filename);
-    });
-        
+    getPdf(htmlContent);
+    })
+
 }
+
+function getPdf(htmlContent){
+    //console.log("HTML ",htmlContent);
+    //create html file and write the htmlcontent which we are getting from Mustache template
+    fs.writeFile('output.html',htmlContent,function (err) {
+      if (err){
+        console.log(err)
+        return;
+      }
+    })
+    //convert the HTML Content/HTML Page to PDF
+    htmlToPdf.convertHTMLFile('output.html', 'output.pdf',
+    function (error, data) {
+       if (error) {
+            console.log("Error Occured ",error);
+            return;
+        }
+        console.log("PDF Succesfully saved in "+process.cwd()+"\\output.pdf");
+    })        
+}
+
+
 
 
