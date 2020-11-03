225911081292a2002df916feddbea097f1dc9bf6
Tolga Erdönmez <45141388+tolgaerdonmez@users.noreply.github.com>
2020-11-04 00:50:34 +0300

    added logger helper to log only when env var DEV=true (#588)
    I tested it and after requiring the logger module it has no errors, but
    there is no type highlight

--- twindle-cli/renderer/md/index.js
+++ twindle-cli/renderer/md/index.js
@@ -1,8 +1,11 @@
-const markdown = (name , tweet , url , username)=>{
-return(
-  '### ' + tweet + '\n'+'![img]('+url+')'+'\n'+name+' | '+username
-)
-}
-console.log(
-  markdown('naval','How to Get Rich (without getting lucky)','https://pbs.twimg.com/profile_images/1256841238298292232/ycqwaMI2_400x400.jpg','@naval')
-)
+const markdown = (name, tweet, url, username) => {
+	return "### " + tweet + "\n" + "![img](" + url + ")" + "\n" + name + " | " + username;
+};
+console.devLog(
+	markdown(
+		"naval",
+		"How to Get Rich (without getting lucky)",
+		"https://pbs.twimg.com/profile_images/1256841238298292232/ycqwaMI2_400x400.jpg",
+		"@naval"
+	)
+);
