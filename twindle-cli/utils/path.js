51e115379bebfecca00f4ce5d883049ccb5e2584
Kenny John Jacob <johnjacobkenny@gmail.com>
2020-11-04 19:31:59 +0530

    Fix kindle email not sent

--- twindle-cli/utils/path.js
+++ twindle-cli/utils/path.js
@@ -1,13 +1,15 @@
 const path = require("path");
 const { getLibraryPath } = require("./library");
 
-const getOutputFilePath = outputFilename => {
-	const filenameEndsInPdf = outputFilename.includes("pdf");
-	let outputFilePath = getLibraryPath(outputFilename);
+const getOutputFilePath = (outputFilename) => {
+  const filenameEndsInPdf = outputFilename.includes("pdf");
+  let outputFilePath = getLibraryPath(outputFilename);
 
-	if (!filenameEndsInPdf) outputFilePath += ".pdf";
+  if (!filenameEndsInPdf) outputFilePath += ".pdf";
 
-	return outputFilePath;
+  return outputFilePath;
 };
 
-module.exports = { getOutputFilePath };
+const getFilenameFromPath = (filename) => filename.split(path.sep).reverse()[0];
+
+module.exports = { getFilenameFromPath, getOutputFilePath };
