2b4aa5f57977d6863e6c4697370bb33aa73b942e
Kenny John Jacob <johnjacobkenny@gmail.com>
2020-11-04 20:18:13 +0530

    Add mock flag

--- twindle-cli/index.js
+++ twindle-cli/index.js
@@ -9,19 +9,28 @@ const { sendToKindle } = require("./utils/send-to-kindle");
 async function main() {
   prepareCli();
 
-  const { format, outputFilename, tweetId, kindleEmail } = getCommandlineArgs(process.argv);
+  const {
+    format,
+    outputFilename,
+    tweetId,
+    kindleEmail,
+    mock,
+  } = getCommandlineArgs(process.argv);
 
   try {
-    const tweets = await getTweetsFromTweetId(tweetId);
+    // this next line is wrong
+    let tweets = require("../twindle-core/twitter_responses/response-version2-tweetthread.json");
+    if (!mock) tweets = await getTweetsFromTweetId(tweetId);
+
     const outputFilePath = getOutputFilePath(outputFilename);
     await Renderer.render(tweets, format, outputFilePath);
 
-		if (kindleEmail) {
-			await sendToKindle(kindleEmail, outputFilePath);
-		}
-	} catch (e) {
-		console.error(e);
-	}
+    if (kindleEmail) {
+      await sendToKindle(kindleEmail, outputFilePath);
+    }
+  } catch (e) {
+    console.error(e);
+  }
 
   // If not for this line, the script never finishes
   process.exit();
