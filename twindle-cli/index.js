41af51b69a3c7da57d309a40004e932ab241284a
Vijaya Bhaskar <5336488+vijaya-bhaskar@users.noreply.github.com>
2020-11-06 02:32:46 +0530

    Added Puppeteer WebScraping Integration (#639)
    Co-authored-by: Vijaya Ranguri <vijaya.ranguri@ggktech.com>

--- twindle-cli/index.js
+++ twindle-cli/index.js
@@ -1,11 +1,12 @@
 // Entry program
-require("./helpers/logger");
-require("dotenv").config();
-const { getCommandlineArgs, prepareCli } = require("./cli");
-const Renderer = require("./renderer");
-const { getTweetsFromTweetId } = require("./twitter");
-const { getOutputFilePath } = require("./utils/path");
-const { sendToKindle } = require("./utils/send-to-kindle");
+require('./helpers/logger');
+require('dotenv').config();
+const { getCommandlineArgs, prepareCli } = require('./cli');
+const Renderer = require('./renderer');
+const { getTweetsFromTweetId } = require('./twitter');
+const { getOutputFilePath } = require('./utils/path');
+const { sendToKindle } = require('./utils/send-to-kindle');
+const { getTweet } = require('./twitter-puppeteer');
 
 async function main() {
   prepareCli();
@@ -16,12 +17,17 @@ async function main() {
     tweetId,
     kindleEmail,
     mock,
+    url,
+    shouldUsePuppeteer,
   } = getCommandlineArgs(process.argv);
 
   try {
     // this next line is wrong
-    let tweets = require("./twitter/twitter_responses/response-version2-tweetthread.json");
-    if (!mock) tweets = await getTweetsFromTweetId(tweetId);
+    let tweets = require('./twitter/twitter_responses/response-version2-tweetthread.json');
+    if (!mock) {
+      if (shouldUsePuppeteer) tweets = await getTweet(url);
+      else tweets = await getTweetsFromTweetId(tweetId);
+    }
 
     const outputFilePath = getOutputFilePath(outputFilename);
     await Renderer.render(tweets, format, outputFilePath);
