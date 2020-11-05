2e48825e497d4f72d9b87c5c5daf715bf30463a0
Puru Vijay <47742487+PuruVJ@users.noreply.github.com>
2020-11-05 16:46:28 +0530

    Change some layout, add rulers (#634)
    * Change some layout, add rulers
    
    * Bigger font
    
    * Fixes

--- twindle-cli/renderer/pdf/create-pdf.js
+++ twindle-cli/renderer/pdf/create-pdf.js
@@ -40,10 +40,10 @@ async function createPdf(outputPath, htmlContent) {
       path: outputPath,
       format: "A5",
       margin: {
-        bottom: 40, // minimum required for footer msg to display
-        left: 25,
-        right: 35,
-        top: 30,
+        bottom: 52, // minimum required for footer msg to display
+        left: 20,
+        right: 20,
+        top: 10,
       },
       printBackground: true,
       displayHeaderFooter: true,
