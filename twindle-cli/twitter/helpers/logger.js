ef2d751567257a88f561669e681d6f2ea869f752
Puru Vijay <awesomepuruvj@gmail.com>
2020-11-07 16:27:55 +0530

    SOme simple cleanup and putting stuff where it belongs

--- twindle-cli/twitter/helpers/logger.js
+++
@@ -1,9 +0,0 @@
-function devLog(...args) {
-	if (process.env.DEV === "true") {
-		console.log(...args);
-	}
-}
-
-// this adds a new function to console which only prints the input
-// when environment variable DEV=true
-console.devLog = devLog;
