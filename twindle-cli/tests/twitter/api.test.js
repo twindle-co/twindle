81805f5638329785505ff3f0360138d0b6109aad
Kenny John Jacob <johnjacobkenny@gmail.com>
2020-11-10 20:52:09 +0530

    Add link to docs for async jest test

--- twindle-cli/tests/twitter/api.test.js
+++ twindle-cli/tests/twitter/api.test.js
@@ -7,6 +7,7 @@ describe("api module", () => {
     expect(() => getConversationById()).toThrow(ApiErrors.TweetIDNotProvidedError);
   });
 
+  // see https://github.com/facebook/jest/issues/5538#issuecomment-461013424
   it("should throw an error if called without token", async () => {
     await expect(() => getTweetById("1324263512621883393")).rejects.toThrow(
       ApiErrors.TokenNotProvidedError
