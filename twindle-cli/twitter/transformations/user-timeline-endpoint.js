7f424e53141fdb67bdaaf866ee44c6eda99f002b
Mira-Alf <mysvadhyaya@gmail.com>
2020-11-11 22:53:52 +0530

    Fetching user timeline tweets
    * Introduced two new command line options u for userId and n for number of tweets(default value 10)
    * If instead of the id, the u option is provided, then user's recent tweets are fetched
    * Lots of testing needs to be done for how the tweets and their replies are displayed - I am ignoring retweets or replies to other users or tweets having any user mentions for now. These can be added down the road if necessary - any PDF structure change necessary needs to be accomplished by creating a separate template
    * richRendering was causing some weird bugs - I have tried my best to fix them but puru needs to take a look at the changes to see if they are correct.
    * Since the API endpoint used is a search endpoint only tweets made by the user in the last 7 days will be displayed. How to handle no tweets scenario??

---
+++ twindle-cli/twitter/transformations/user-timeline-endpoint.js
@@ -0,0 +1,52 @@
+const { createCustomTweet } = require("./helpers");
+
+const { renderRichTweets, fixUserDescription } = require("./rich-rendering");
+
+const { formatTimestamp } = require("../utils/date");
+
+/**
+ * @param {TwitterConversationResponse} responseJSON
+ */
+async function processUserTweets(screenName, responseJSON, token) {
+  const tweets = (responseJSON.data || []).map((resData) => ({
+    ...resData,
+    includes: responseJSON.includes,
+  }));
+
+  const userObject = responseJSON.includes.users
+                    .filter((user)=>user.username === screenName)[0];
+
+  let user = userObject;
+
+  /** @type {CustomTweetsObject} */
+  let resp = {
+    data: [],
+    common: {},
+  };
+
+  resp.common.created_at = formatTimestamp(user.created_at);
+  resp.common.user = { ...user };
+
+  resp = fixUserDescription(resp);
+
+  resp.common.user.profile_image_url = resp.common.user.profile_image_url.replace("_normal.", ".");
+
+
+
+  let conversations = [];
+  for (let tweet of tweets) {
+    if(!conversations.includes(tweet.conversation_id))  {
+      conversations.push(tweet.conversation_id);
+      let threadTweets = tweets.filter((t)=>tweet.conversation_id === t.conversation_id);
+      for(i = threadTweets.length-1; i >= 0; i--) {
+        resp.data.push(createCustomTweet(await renderRichTweets(threadTweets[i], token)));
+      }
+    }
+  }
+  resp.common.count = resp.data.length;
+  return resp;
+}
+
+module.exports = {
+  processUserTweets,
+};
