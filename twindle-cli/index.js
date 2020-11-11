c63dec9cc972509d61b47c5cd16c16f2b4f8ed79
Kryptöñ <madhusudanbabar@gmail.com>
2020-11-11 15:29:03 +0530

    improved file name  (#763)
    * fixed filenames
    
    removed the @ symbol from the filename
    
    * add - appendToFilename option
    
    * improved pdf filename
    
    * add - null check for the append string

--- twindle-cli/index.js
+++ twindle-cli/index.js
@@ -22,6 +22,7 @@ async function main() {
     kindleEmail,
     mock,
     shouldUsePuppeteer,
+    appendToFilename
   } = getCommandlineArgs(process.argv);
 
   try {
@@ -40,7 +41,7 @@ async function main() {
     }-${
       (tweets && tweets.common && tweets.common.created_at.replace(/,/g, "").replace(/ /g, "-")) ||
       "thread"
-    }`;
+    }${ appendToFilename ? "-" +appendToFilename : ""}`;
 
     const outputFilePath = getOutputFilePath(outputFilename || intelligentOutputFileName, format);
     await Renderer.render(tweets, format, outputFilePath);
