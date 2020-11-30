8b1a1a3f6084b8a8fde8bcc85f712a087c076a25
Puru Vijay <awesomepuruvj@gmail.com>
2020-11-30 14:21:19 +0530

    Add slots to the button

--- twindle-thread/src/App.svelte
+++ twindle-thread/src/App.svelte
@@ -1,7 +1,16 @@
 <script>
   import Button from './components/Button.svelte';
+  import AppIcon from './components/utils/AppIcon.svelte';
+
+  import { mdiPlus } from '@mdi/js';
 </script>
 
 <Button on:click={(e) => console.log(e)} color="primary" variant="contained" class="hello">
+  <span slot="icon-start">
+    <AppIcon size={20} path={mdiPlus} />
+  </span>
   Hello
+  <span slot="icon-end">
+    <AppIcon size={20} path={mdiPlus} />
+  </span>
 </Button>
--- twindle-thread/src/components/utils/AppIcon.svelte
+++ twindle-thread/src/components/utils/AppIcon.svelte
@@ -9,10 +9,10 @@
 </script>
 
 <svg
-  {...$$props}
   xmlns="http://www.w3.org/2000/svg"
   width={size}
   height={size}
-  viewBox={`0 0 24 24`}>
+  viewBox={`0 0 24 24`}
+  {...$$props}>
   <path d={path} />
 </svg>
