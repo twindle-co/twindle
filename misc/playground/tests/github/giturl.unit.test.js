8badb2e12be6f1783583955fefd55602a2c7d702
Puru Vijay <47742487+PuruVJ@users.noreply.github.com>
2020-12-20 17:34:24 +0530

    Add basic tests in Twitter folder (#974)
    * x
    
    * Add some test cases
    
    * Move old tests to playground
    
    * Remove unnecessary changes

---
+++ misc/playground/tests/github/giturl.unit.test.js
@@ -0,0 +1,8 @@
+const { validateGithubURL } = require("../../../../twindle-cli/src/cli");
+
+const mockURL =
+  "https://github.com/getify/You-Dont-Know-JS/blob/2nd-ed/README.html";
+
+test("verify github url extension", async () => {
+  await expect(() => validateGithubURL(mockURL)).rejects.toThrow(Error);
+});
