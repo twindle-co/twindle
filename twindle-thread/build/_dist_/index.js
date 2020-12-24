e9bf3d33e2ca47d784a062b620447d1cd61f3bac
Puru Vijay <devpuruvj@gmail.com>
2020-12-24 17:38:20 +0530

    Delete those unnecesarry files

--- twindle-thread/build/_dist_/index.js
+++
@@ -1,1 +0,0 @@
-const t={};import p from"../__snowpack__/env.js";t.env=p;import e from"./App.js";var o=new e({target:document.body});export default o;t.hot&&(t.hot.accept(),t.hot.dispose(()=>{o.$destroy()}));
