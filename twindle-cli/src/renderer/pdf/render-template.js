00631581e1ba218c8475d1fe6281ff2b558834f4
Puru Vijay <devpuruvj@gmail.com>
2020-12-24 17:34:53 +0530

    Hackernews is now Svelte-y

--- twindle-cli/src/renderer/pdf/render-template.js
+++ twindle-cli/src/renderer/pdf/render-template.js
@@ -4,9 +4,9 @@ const { writeFile, readFile } = require("fs").promises;
 const { render } = require("../main");
 const { tmpdir } = require("os");
 const { join } = require("path");
-const hbs = require("handlebars");
 const GithubApp = require("../main/github/components/App.svelte").default;
 const MozReadabilityApp = require("../main/article/components/App.svelte").default;
+const HackerNewsApp = require("../main/hackernews/components/App.svelte").default;
 
 /**
  * Renders the html template with the given data and returns the html string
@@ -69,49 +69,27 @@ async function renderGithubTemplate(data) {
 }
 
 async function renderHackernewsTemplate(data) {
-  const threadsHtml = await readFile(
-    `${__dirname}/../main/hackernews/threads-template.hbs`,
-    "utf-8"
-  );
-  const articlesHtml = await readFile(
-    `${__dirname}/../main/hackernews/articles-partial.hbs`,
-    "utf-8"
-  );
-  const commonInfoHtml = await readFile(
-    `${__dirname}/../main/hackernews/common-info-partial.hbs`,
-    "utf-8"
-  );
-  const commentHtml = await readFile(
-    `${__dirname}/../main/hackernews/comment-partial.hbs`,
-    "utf-8"
-  );
-  const css = await readFile(`${__dirname}/../main/hackernews/style.css`, "utf-8");
+  // @ts-ignore
+  let { html, css } = HackerNewsApp.render(data);
 
-  hbs.registerPartial("articles-partial", articlesHtml);
-  hbs.registerPartial("common-info-partial", commonInfoHtml);
-  hbs.registerPartial("comment-partial", commentHtml);
-  hbs.registerPartial("style", css);
-  hbs.registerHelper("levelcalculator", function (comment) {
-    if (comment.level - 1 == 0 && comment.index == 0) return "";
-    else if (comment.level - 1 == 0 && comment.index != 0) {
-      if (comment.comments.length > 0) return "style='page-break-before:always;'";
-      else return "style='border-top: 1px solid #ddd;'";
-    } else if (comment.level - 1 > 0) return "style='padding-left:35px'";
-  });
+  html = `<!doctype html> 
+          <html> 
+            <head>
+              <meta charset="UTF-8" /
+              <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+              <style>${css.code}</style>
+            </head>
+            <body>
+              ${html}
+            </body>
+          </html>
+`;
 
-  // creates the Handlebars template object
-  const template = hbs.compile(threadsHtml, {
-    strict: true,
-  });
-
-  // renders the html template with the given data
-  const rendered = template(data);
-
-  const tmpPath = join(tmpdir(), "hello.html");
-  await writeFile(tmpPath, rendered, "utf-8");
-  await writeFile(tmpdir() + "/x.json", JSON.stringify(data, null, 2), "utf-8");
+  const tmpPath = join(tmpdir(), "hackernews.html");
+  await writeFile(tmpPath, html, "utf-8");
+  await writeFile(tmpdir() + "/hackernews.json", JSON.stringify(data, null, 2), "utf-8");
   console.devLog("rendered saved to ", tmpPath);
-  return rendered;
+  return html;
 }
 
 async function renderArticleTemplate(data) {
